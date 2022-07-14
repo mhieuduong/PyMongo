e6a22bcf033163519b615ae901a712f89bb9030e
mhieu <mhieuduong@gmail.com>
2022-07-15 06:40:25 +0700

    early

---
+++ app.py
@@ -0,0 +1,48 @@
+import pprint
+from pymongo import mongo_client
+from pymongo.server_api import ServerApi
+
+
+"""
+1. Hien thi danh sach sinh vien
+2. Tao moi sinh vien
+3. Cap nhat diem sinh vien
+4. Xoa sinh vien
+5. Kiem tra sinh vien du diem qua mon
+"""
+"""
+Sinh vien
+    Ho ten
+    Ngay sinh
+    MSV
+    Diem mon hoc: GDQP, Dien Kinh, Bong Chuyen
+    Gioi tinh
+"""
+
+
+def connect_to_mongo():
+    pass
+
+
+def show_collection():
+    pass
+
+
+def create_new_student():
+    pass
+
+
+def update_scores():
+    pass
+
+
+def delete_a_student():
+    pass
+
+
+def calculate_score():
+    pass
+
+
+if __name__ == '__main__':
+    print('Hello')
